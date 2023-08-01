f44e06f3b23d9373b7abca2348709bcf623fc0cb
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-01 11:28:21 +0330

    Added Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+
+const Header = () => {
+  return (
+    <Stack spacing={2}>
+      <Typography>Hearder</Typography>
+    </Stack>
+  );
+};
+
+export default Header;
