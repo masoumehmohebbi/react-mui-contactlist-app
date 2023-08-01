b033442749e001cdb783fa48f9191a33f9a699c8
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-01 11:28:35 +0330

    Added Layout.jsx

---
+++ src/layout/layout.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
