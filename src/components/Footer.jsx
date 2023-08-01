73eb166c320efa054b64c2d3d9e0830a4f293bec
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-01 11:28:04 +0330

    Added Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+
+const Footer = () => {
+  return (
+    <Stack spacing={2}>
+      <Typography>Footer</Typography>
+    </Stack>
+  );
+};
+
+export default Footer;
