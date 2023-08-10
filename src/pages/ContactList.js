b9d0ca287d9c7076e6e5a1ff74cb3e78a958a504
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-10 18:16:44 +0330

    Added Add Contact Text

--- src/pages/ContactList.js
+++ src/pages/ContactList.js
@@ -61,153 +61,179 @@ export default function ContactList({
         contacts={contacts}
       />
       <Box my={7} component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Grid container spacing={2}>
-          {contacts ? (
-            contacts.map((contact) => (
-              <Grid xs={12} sm={6} md={4} lg={3} key={contact.id}>
-                <Card
-                  sx={{
-                    maxWidth: 197,
-                    marginX: "auto",
-                  }}
-                >
-                  <CardMedia
+        {contacts.length === 0 ? (
+          <Typography
+            sx={{
+              fontWeight: "600",
+              fontSize: "22px",
+              textAlign: "center",
+              paddingTop: "40px",
+            }}
+          >
+            Add Contact!
+          </Typography>
+        ) : (
+          <Grid container spacing={2}>
+            {contacts ? (
+              contacts.map((contact) => (
+                <Grid xs={12} sm={6} md={4} lg={3} key={contact.id}>
+                  <Card
                     sx={{
-                      // height: 160,
-                      height: 110,
-                      backgroundPositionY: "1px",
-                      width: "95px",
-                      margin: "auto",
-                    }}
-                    image="https://th.bing.com/th/id/R.d9d19204023a46ff71baecd91ff5420c?rik=rVs%2f4D7ex%2fEbSw&riu=http%3a%2f%2fwww.newdesignfile.com%2fpostpic%2f2011%2f10%2fwindows-8-default-user-icon_71046.png&ehk=wRV87OUeTL%2f2EOTxy%2fsRnjiJRFLnpFqFySsG972evMA%3d&risl=&pid=ImgRaw&r=0"
-                    title="green iguana"
-                  />
-                  <CardContent>
-                    <Typography
-                      variant="body2"
-                      color="text.secondary"
-                      sx={{
-                        display: "flex",
-                        alignItems: "start",
-                        gap: "5px",
-                      }}
-                    >
-                      <AccessTimeFilled
-                        sx={{ color: "#84cc16", fontSize: "18px" }}
-                      />
-                      <Typography
-                        component="span"
-                        sx={{ color: "#65a30d", fontWeight: "600" }}
-                      >
-                        {new Date().toLocaleString() + ""}
-                      </Typography>
-                    </Typography>
-                    <Typography
-                      sx={{ display: "flex", gap: "9px" }}
-                      gutterBottom
-                      variant="h5"
-                      component="div"
-                    >
-                      <Typography
-                        component="span"
-                        sx={{ fontWeight: "800", mt: "11px", color: "#14532d" }}
-                      >
-                        {contact.firstname}
-                      </Typography>
-                      <Typography
-                        component="span"
-                        sx={{ fontWeight: "800", mt: "11px", color: "#14532d" }}
-                      >
-                        {contact.lastname}
-                      </Typography>
-                    </Typography>
-                  </CardContent>
-
-                  <CardActions
-                    sx={{
-                      display: "flex",
-                      justifyContent: "between",
-                      width: "100%",
-                      gap: "1px",
+                      maxWidth: 197,
+                      marginX: "auto",
                     }}
                   >
-                    <Typography component="div">
-                      {contact.gender === "female" ? (
-                        <Tooltip title="Female">
-                          <Female sx={{ color: "#dc2626", fontSize: "24px" }} />
-                        </Tooltip>
-                      ) : (
-                        <Tooltip title="Male">
-                          <Male sx={{ color: "#2563eb", fontSize: "24px" }} />
-                        </Tooltip>
-                      )}
-                    </Typography>
-                    <Box
+                    <CardMedia
+                      sx={{
+                        // height: 160,
+                        height: 110,
+                        backgroundPositionY: "1px",
+                        width: "95px",
+                        margin: "auto",
+                      }}
+                      image="https://th.bing.com/th/id/R.d9d19204023a46ff71baecd91ff5420c?rik=rVs%2f4D7ex%2fEbSw&riu=http%3a%2f%2fwww.newdesignfile.com%2fpostpic%2f2011%2f10%2fwindows-8-default-user-icon_71046.png&ehk=wRV87OUeTL%2f2EOTxy%2fsRnjiJRFLnpFqFySsG972evMA%3d&risl=&pid=ImgRaw&r=0"
+                      title="green iguana"
+                    />
+                    <CardContent>
+                      <Typography
+                        variant="body2"
+                        color="text.secondary"
+                        sx={{
+                          display: "flex",
+                          alignItems: "start",
+                          gap: "5px",
+                        }}
+                      >
+                        <AccessTimeFilled
+                          sx={{ color: "#84cc16", fontSize: "18px" }}
+                        />
+                        <Typography
+                          component="span"
+                          sx={{ color: "#65a30d", fontWeight: "600" }}
+                        >
+                          {new Date().toLocaleString() + ""}
+                        </Typography>
+                      </Typography>
+                      <Typography
+                        sx={{ display: "flex", gap: "9px" }}
+                        gutterBottom
+                        variant="h5"
+                        component="div"
+                      >
+                        <Typography
+                          component="span"
+                          sx={{
+                            fontWeight: "800",
+                            mt: "11px",
+                            color: "#14532d",
+                          }}
+                        >
+                          {contact.firstname}
+                        </Typography>
+                        <Typography
+                          component="span"
+                          sx={{
+                            fontWeight: "800",
+                            mt: "11px",
+                            color: "#14532d",
+                          }}
+                        >
+                          {contact.lastname}
+                        </Typography>
+                      </Typography>
+                    </CardContent>
+
+                    <CardActions
                       sx={{
                         display: "flex",
-                        justifyContent: "end",
+                        justifyContent: "between",
                         width: "100%",
-                        gap: "6px",
+                        gap: "1px",
                       }}
                     >
-                      <Tooltip title="Detail">
-                        <RemoveRedEye
-                          onClick={() =>
-                            navigate(`/user/${contact.id}`, {
-                              state: { contact: contact },
-                            })
-                          }
-                          sx={{
-                            "&:hover": {
-                              color: "#166534",
-                            },
-                            cursor: "pointer",
-                            color: "#4f46e5",
-                            fontSize: "19px",
-                          }}
-                        />
-                      </Tooltip>
-
-                      <Tooltip title="Edit">
-                        <Edit
-                          onClick={() => navigate(`/edit/${contact.id}`)}
-                          sx={{
-                            "&:hover": {
-                              color: "#166534",
-                            },
-                            cursor: "pointer",
-                            color: "#16a34a",
-                            fontSize: "19px",
-                          }}
-                        />
-                      </Tooltip>
-
-                      <Tooltip
-                        title="Delete"
-                        onClick={() => deleteOneContactHandler(contact.id)}
+                      <Typography component="div">
+                        {contact.gender === "female" ? (
+                          <Tooltip title="Female">
+                            <Female
+                              sx={{ color: "#dc2626", fontSize: "24px" }}
+                            />
+                          </Tooltip>
+                        ) : (
+                          <Tooltip title="Male">
+                            <Male sx={{ color: "#2563eb", fontSize: "24px" }} />
+                          </Tooltip>
+                        )}
+                      </Typography>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          justifyContent: "end",
+                          width: "100%",
+                          gap: "6px",
+                        }}
                       >
-                        <RestoreFromTrash
-                          sx={{
-                            "&:hover": {
-                              color: "#166534",
-                            },
-                            cursor: "pointer",
-                            color: "#e11d48",
-                            fontSize: "19px",
-                          }}
-                        />
-                      </Tooltip>
-                    </Box>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))
-          ) : (
-            <Typography sx={{ width: "100%", bgcolor: "red" }} component="span">
-              No Contacts Yet!
-            </Typography>
-          )}
-        </Grid>
+                        <Tooltip title="Detail">
+                          <RemoveRedEye
+                            onClick={() =>
+                              navigate(`/user/${contact.id}`, {
+                                state: { contact: contact },
+                              })
+                            }
+                            sx={{
+                              "&:hover": {
+                                color: "#166534",
+                              },
+                              cursor: "pointer",
+                              color: "#4f46e5",
+                              fontSize: "19px",
+                            }}
+                          />
+                        </Tooltip>
+
+                        <Tooltip title="Edit">
+                          <Edit
+                            onClick={() => navigate(`/edit/${contact.id}`)}
+                            sx={{
+                              "&:hover": {
+                                color: "#166534",
+                              },
+                              cursor: "pointer",
+                              color: "#16a34a",
+                              fontSize: "19px",
+                            }}
+                          />
+                        </Tooltip>
+
+                        <Tooltip
+                          title="Delete"
+                          onClick={() => deleteOneContactHandler(contact.id)}
+                        >
+                          <RestoreFromTrash
+                            sx={{
+                              "&:hover": {
+                                color: "#166534",
+                              },
+                              cursor: "pointer",
+                              color: "#e11d48",
+                              fontSize: "19px",
+                            }}
+                          />
+                        </Tooltip>
+                      </Box>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))
+            ) : (
+              <Typography
+                sx={{ width: "100%", bgcolor: "red" }}
+                component="span"
+              >
+                No Contacts Yet!
+              </Typography>
+            )}
+          </Grid>
+        )}
       </Box>
     </Box>
   );
