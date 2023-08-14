ecc749d1e45aca4a69ce511e2745eafa52ce0006
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-14 17:35:11 +0330

    Added addOneContact.js

--- src/services/addContactService.js
+++ src/services/addContactService.js
@@ -1,7 +1,7 @@
-import http from "./httpService";
+import supabase from "../supabase";
 
 const addOneContact = (data) => {
-  return http.post(`/contacts`, data);
+  return supabase.from("contactlist").insert([data]).select();
 };
 
 export default addOneContact;
