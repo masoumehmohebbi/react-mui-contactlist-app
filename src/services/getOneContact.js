047e8d83b106e165e970d5b826c88d064b5d1926
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-14 16:15:16 +0330

    Added getOneContact.js

--- src/services/getOneContact.js
+++ src/services/getOneContact.js
@@ -1,7 +1,9 @@
-import http from "./httpService";
+import supabase from "../supabase";
+// import http from "./httpService";
 
 const getOneContact = (id) => {
-  return http.get(`/contacts/${id}`);
+  // return http.get(`/contacts/${id}`);
+  return supabase.from("contactlist").select("*").filter("id", "eq", id);
 };
 
 export default getOneContact;
