34ea2e449ddb31af9b9509f8187da2045befc62f
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-14 17:36:30 +0330

    Added UpdateContact.js

--- src/services/updateContact.js
+++ src/services/updateContact.js
@@ -1,7 +1,16 @@
-import http from "./httpService";
-
-const updateContact = (id, data) => {
-  return http.put(`/contacts/${id}`, data);
+import supabase from "../supabase";
+const updateContact = (id, contact) => {
+  return supabase
+    .from("contactlist")
+    .update({
+      firstname: contact.firstname,
+      lastname: contact.lastname,
+      phonenumber: contact.phonenumber,
+      email: contact.email,
+      gender: contact.gender,
+      relationship: contact.relationship,
+    })
+    .eq("id", id);
 };
 
 export default updateContact;
